4f7bbdf23288c6d7b34f7b59a348e6ada69c92a2
Shifty-z <42383109+Shifty-z@users.noreply.github.com>
2026-03-02 06:37:48 -0500

    Build: RM Stale Bins
    Updates build script, so previously built binaries are removed; relies on rm.

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# WARNING: This will delete any existing string-replace binaries in the current
+# 	directory!
+rm -f string-replace
+
 # Nobody wants to remember how to build something, so just run this
 # to make a binary. Odin will handle the rest for platforms you care about.
 odin build . -o:speed
